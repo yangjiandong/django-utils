dc686157698ae1ea98094f87dd146233ae18eb6c
Charles Leifer <coleifer@gmail.com>
2011-06-03 10:54:32 -0500

    Make things work if pygments isn't installed

--- djutils/utils/highlighter.py
+++ djutils/utils/highlighter.py
@@ -1,13 +1,17 @@
-from pygments import formatters, lexers
-from pygments import highlight as syntax_highlight
-from pygments.lexers import guess_lexer, get_lexer_by_name
+try:
+    from pygments import formatters, lexers
+    from pygments import highlight as syntax_highlight
+    from pygments.lexers import guess_lexer, get_lexer_by_name
 
-def highlight(data, language=None, default='python'):
-    """Simple wrapper around pygments"""
-    try:
-        lexer = get_lexer_by_name(language, stripall=True, encoding='UTF-8')
-    except ValueError:
-	    lexer = get_lexer_by_name(default, stripall=True, encoding='UTF-8')
-	
-    formatter = formatters.HtmlFormatter()
-    return syntax_highlight(data, lexer, formatter)
+    def highlight(data, language=None, default='python'):
+        """Simple wrapper around pygments"""
+        try:
+            lexer = get_lexer_by_name(language, stripall=True, encoding='UTF-8')
+        except ValueError:
+            lexer = get_lexer_by_name(default, stripall=True, encoding='UTF-8')
+        
+        formatter = formatters.HtmlFormatter()
+        return syntax_highlight(data, lexer, formatter)
+
+except ImportError:
+    pass
