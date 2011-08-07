e0b3eae92dd2b685ba1f5a74c7e117939dc3d880
Charles Leifer <coleifer@gmail.com>
2011-08-07 14:44:41 -0500

    Conditional import

--- djutils/tests/__init__.py
+++ djutils/tests/__init__.py
@@ -3,6 +3,11 @@ from djutils.tests.context_processors import *
 from djutils.tests.decorators import *
 from djutils.tests.db import *
 from djutils.tests.middleware import *
-from djutils.tests.queue import *
 from djutils.tests.templatetags import *
 from djutils.tests.utils import *
+
+try:
+    from djutils.tests.queue import *
+except ImportError:
+    import sys
+    sys.stderr.write('Unable to import queue tests, skipping')
